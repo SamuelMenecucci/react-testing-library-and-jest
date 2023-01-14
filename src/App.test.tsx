67c7a4195d98cb7c0585d895348f764ceb95c511
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2023-01-14 19:09:10 -0300

    feat: testing if text and color changes when clicked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { logRoles } from "@testing-library/dom";
 import App from "./App";
 
@@ -14,8 +14,14 @@ test("button has correct initial color", () => {
 
   //expect the background color to be red
   expect(colorButton).toHaveStyle({ "background-color": "red" }); //if I pass the key with camelCase and any thing as value,  the test will give me a false positive. this is a known issue with jest-dom, reported May 19, 2022 -- even though the fix for a similar issue was supposedly merged in Oct 2019. The Workaround: use kebab case. the test will run correctly when the css property is specified with kebab case instead of camel case.
+
+  //click button
+  //fireEvent help us to interact with the elements in our virtual DOM
+  fireEvent.click(colorButton);
+
+  //expect the background color to be blue
+  expect(colorButton).toHaveStyle({ "background-color": "blue" });
+
+  //expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent("Change to red"); //checking the content text inside the button
 });
-
-test("button has the correct initial text", () => {});
-
-test("button turns blue when clicked", () => {});
