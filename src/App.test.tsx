81c14de399de176ddca87666cde63c3b79ca4a84
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2023-01-14 18:49:12 -0300

    feat: testing the style and the displaying text of a button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { logRoles } from "@testing-library/dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />); //the first thing that happens in the test function is we run this render method. the render method creates a virtual dom for whatever jsx/tsx you give it as the argument. here the argument is the tsx app component.
-  const linkElement =
-    //screen is a global object that I can use to access the virtual DOM and select the elements with the queries.
-    screen.getByRole("link", { name: /learn react/i }); //as the element that have the Learn React text is an anchor, i can select by using getByRole, passing the role link.
-  expect(linkElement).toBeInTheDocument();
+test("button has correct initial color", () => {
+  const { container } = render(<App />);
+
+  //if i'm not sure what role an element have, I can use the logRoles
+  logRoles(container);
+
+  //find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ "background-color": "red" }); //if I pass the key with camelCase and any thing as value,  the test will give me a false positive. this is a known issue with jest-dom, reported May 19, 2022 -- even though the fix for a similar issue was supposedly merged in Oct 2019. The Workaround: use kebab case. the test will run correctly when the css property is specified with kebab case instead of camel case.
 });
+
+test("button has the correct initial text", () => {});
+
+test("button turns blue when clicked", () => {});
